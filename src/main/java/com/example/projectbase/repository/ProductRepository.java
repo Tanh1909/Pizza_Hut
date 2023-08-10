80971def99165fffc664b994c1fc1e6c78e91981
dung8703 <nguyenhoangdung8703@gmail.com>
2023-08-10 21:12:32 +0700

    update_get_product

--- src/main/java/com/example/projectbase/repository/ProductRepository.java
+++ src/main/java/com/example/projectbase/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.example.projectbase.repository;
 
 import com.example.projectbase.domain.entity.ProductEntity;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,7 +9,7 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     @Query("SELECT p FROM ProductEntity p WHERE p.categoryEntity.id = :categoryId")
-    List<ProductEntity> findByCategoryId(Long categoryId);
+    List<ProductEntity> findByCategoryId(@Param("categoryId") Long categoryId);
     @Query(value = "select p.* FROM products p inner join combo_item cb ON p.id=cb.product_id where combo_id=?1 &&category_id=?2 ",nativeQuery = true)
     List<ProductEntity> findByCombo(Long comboId,Long categoryId);
     @Query(value = "select p.* FROM products p where p.point != 0",nativeQuery = true)
