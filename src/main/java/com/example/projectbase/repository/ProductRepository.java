ca90827163aa7c928b2e78d25b1ba3397b1e46b0
dung8703 <nguyenhoangdung8703@gmail.com>
2023-08-10 21:30:23 +0700

    update_get_product

--- src/main/java/com/example/projectbase/repository/ProductRepository.java
+++ src/main/java/com/example/projectbase/repository/ProductRepository.java
@@ -1,9 +1,9 @@
 package com.example.projectbase.repository;
 
 import com.example.projectbase.domain.entity.ProductEntity;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
